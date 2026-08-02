5abaeae0d6321e742e32d8193fef580b2539c920
Agatha Almeida <seu-email@gmail.com>
2026-08-02 10:22:25 -0300

    feat:implementando o sistema de templates com jinja2

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+#funcao template,responsavel por renderizar templates
 
 app = Flask(__name__)
 #criando a aplicacao flask que vai me permitir ter servidor,rotas,backend
 
 @app.route("/")
 def home():
-    return "Sistema Gerenciador de Tarefas"
-
-
+    return render_template("index.html",titulo="Página Inicial")
+#fazendo uma mudancao de cada vez, começando pela pagina inicial
+#o titulo agora nao esta mais gravado no html,ele veio propriamentedo backend
 @app.route("/tarefas", methods=["GET"])
 def listar_tarefas():
-    return "Lista de tarefas"
+    return render_template("tarefas.html",titulo="Lista de Tarefas")
 
 
 @app.route("/tarefas/nova")
 def nova_tarefa():
-    return "Criar nova tarefa"
+    return render_template("nova_tarefa.html",titulo="Nova Tarefa")
+
+#o que deve ser percebido:
+#todas as paginas possuem o  mesmo cabeçalho
+#todas usam a mesma estrutura
+#apenas o conteudo central muda
+#o titulo da aba do navegador muda conforme a rota
+#objetivo:evitar duplicidade e centralizar aquilo que é comum
